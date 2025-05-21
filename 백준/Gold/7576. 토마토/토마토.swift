8f0ab650a273b0c8ba679b12074af80182746df1
rrheon <121111413+rrheon@users.noreply.github.com>
2025-05-21 23:07:34 +0900

    [Gold V] Title: 토마토, Time: 288 ms, Memory: 120068 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let m = input[0], n = input[1]
+var graph: [[Int]] = []
+let dx = [1, 0, -1, 0]
+let dy = [0, 1, 0, -1]
+
+for _ in 0..<n {
+    graph.append(readLine()!.split(separator: " ").map { Int($0)! })
+}
+
+var queue: [(Int, Int)] = []
+
+for y in 0..<n {
+    for x in 0..<m {
+        if graph[y][x] == 1 {
+            queue.append((y, x))
+        }
+    }
+}
+
+var index = 0
+
+while queue.count > index {
+    let y = queue[index].0
+    let x = queue[index].1
+    
+    for i in 0..<4 {
+        let ny = y + dy[i]
+        let nx = x + dx[i]
+        
+        if 0..<m ~= nx && 0..<n ~= ny && graph[ny][nx] == 0 {
+            graph[ny][nx] = graph[y][x] + 1
+            queue.append((ny, nx))
+        }
+    }
+    index += 1
+}
+
+print(graph.flatMap { $0 }.contains(0) ? -1 : graph.flatMap { $0 }.max()! - 1)
